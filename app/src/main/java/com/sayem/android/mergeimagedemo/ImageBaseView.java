5b94a5c8a1709a3478fa6703ddbe7654ba2fbb67
M.A. Sayem <sayem.cse18@gmail.com>
2016-08-05 18:13:03 +0600

    ondraw added

--- app/src/main/java/com/sayem/android/mergeimagedemo/ImageBaseView.java
+++ app/src/main/java/com/sayem/android/mergeimagedemo/ImageBaseView.java
@@ -2,6 +2,7 @@ package com.sayem.android.mergeimagedemo;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,5 +26,10 @@ public class ImageBaseView extends View {
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public ImageBaseView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
