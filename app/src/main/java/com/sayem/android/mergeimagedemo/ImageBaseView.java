fe66ddb47b38b187a532ebfc7b53312f87109499
M.A. Sayem <sayem.cse18@gmail.com>
2016-08-05 18:09:53 +0600

    base view added
    new class named ImageBaseViewAdded

---
+++ app/src/main/java/com/sayem/android/mergeimagedemo/ImageBaseView.java
@@ -0,0 +1,29 @@
+package com.sayem.android.mergeimagedemo;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by sayem on 8/5/2016.
+ */
+public class ImageBaseView extends View {
+    public ImageBaseView(Context context) {
+        super(context);
+    }
+
+    public ImageBaseView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImageBaseView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ImageBaseView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
