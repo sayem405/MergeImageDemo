803301bca1f4505709f2777043042341723313a3
sayem405 <sayem.cse18@gmail.com>
2016-08-06 21:17:04 +0600

    select from image gallery added

--- app/src/main/java/com/sayem/android/mergeimagedemo/ImageBaseView.java
+++ app/src/main/java/com/sayem/android/mergeimagedemo/ImageBaseView.java
@@ -2,15 +2,23 @@ package com.sayem.android.mergeimagedemo;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by sayem on 8/5/2016.
  */
-public class ImageBaseView extends View {
+public class ImageBaseView extends ImageView {
+
+    private Bitmap mainBitmap;
+    private Bitmap emoBitmap;
+
+
     public ImageBaseView(Context context) {
         super(context);
     }
@@ -31,5 +39,20 @@ public class ImageBaseView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if (emoBitmap != null) {
+
+            canvas.drawBitmap(emoBitmap, 100f, 100f, new Paint(Paint.ANTI_ALIAS_FLAG));
+        }
+    }
+
+
+    public void setEmoBitmap(Bitmap emoBitmap) {
+        this.emoBitmap = emoBitmap;
+        setWillNotDraw(false);
+    }
+
+    public void setMainBitmap(Bitmap mainBitmap) {
+        this.mainBitmap = mainBitmap;
+        setWillNotDraw(false);
     }
 }
